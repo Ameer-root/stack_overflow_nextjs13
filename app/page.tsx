230b015247d91cc140155ccd7e06f7eb474ded6e
Ameer-root <almodrb1@yahoo.com>
2023-10-22 17:19:54 +0200

    implement base tailwind config and style guide

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <h1 className={"h1-bold"}>Hello from nextJs13 Web&apos;Storm</h1>
+      <h2 className={"h2-semibold"}>test</h2>
+    </div>
+  );
 }
