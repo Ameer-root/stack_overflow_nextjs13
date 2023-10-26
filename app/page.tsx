825e5285b89fa17670772ed24e88881a1f86643b
Ameer-root <almodrb1@yahoo.com>
2023-10-26 22:34:23 +0200

    add auth

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className={"h1-bold"}>Hello from nextJs13 Web&apos;Storm</h1>
-      <h2 className={"h2-semibold"}>test</h2>
-    </div>
-  );
-}
